8e85a4d0b26934984da39ee2105bfabf13183da6
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-11 19:18:59 -0300

    telefonico pronto

--- DesafioIphone/Iphone_POO/src/Iphone.java
+++ DesafioIphone/Iphone_POO/src/Iphone.java
@@ -7,46 +7,69 @@ public class Iphone {
         while(true){
             Scanner terminal = new Scanner(System.in);
             try{
-                System.out.println("Acessando as opções do smartphone: \n1- Acessar Reprodutor Musical\n2- Telefônico");
+                System.out.println("Acessando as opções do smartphone: \n1- Acessar Reprodutor Musical\n2- Telefônico\n3- Navegador\n0- desligar");
                 int opcao = terminal.nextInt();
                 switch (opcao) {
                     case 1:
-                        ReprodutorMusical rm = new ReprodutorMusical();
-                        System.out.println("1 - Selecionar Musica \n2-tocar aleatório\n3-pausar");    
-                        int opcaoMusica = terminal.nextInt();
-                        switch (opcaoMusica) {
-                            case 1:
-                                System.out.println();
-                                rm.selecionarMusica();
+                        boolean reprodutorAtivo = true;
+                        while(reprodutorAtivo){
+
+                            ReprodutorMusical rm = new ReprodutorMusical();
+                            System.out.println("1 - Selecionar Musica \n2-tocar aleatório\n3-pausar\n0- voltar");    
+                            int opcaoMusica = terminal.nextInt();
+                            switch (opcaoMusica) {
+                                case 1:
+                                    System.out.println();
+                                    rm.selecionarMusica();
                                 break;
-                            case 2:
-                                rm.tocar();
+                                case 2:
+                                    
                                 break;
-                            case 3:
-                                rm.pausar();
+                                case 3:
+                                    rm.pausar();
                                 break;
-                            default:
+                                case 0:
+                                    reprodutorAtivo = false;
+                                    break;
+                                default:
                                 throw new AssertionError();
+                            }
                         }
                         break;
                     case 2:
-                    Telefone tl = new Telefone();
-                        System.out.println("1- Ligar \n2- Atender \n3- Iniciar CorrerioVoz");
-                        int opcaoTelefonico = terminal.nextInt();
-                        switch (opcaoTelefonico) {
-                            case 1:
-                                
+                        boolean telefoneAtivo = true;
+                        while(telefoneAtivo){
+
+                            Telefone tl = new Telefone();
+                            System.out.println("1- Ligar \n2- Atender \n3- Iniciar CorrerioVoz\n0-voltar");
+                            int opcaoTelefonico = terminal.nextInt();
+                            switch (opcaoTelefonico) {
+                                case 1:
+                                    tl.ligar();
                                 break;
-                            case 2:
-                                
+                                case 2:
+                                    tl.atender();
                                 break;
-                            case 3:
-                                
+                                case 3:
+                                    tl.iniciarCorreioVoz();
                                 break;
-                            default:
+                                case 0:
+                                    telefoneAtivo = false;
+                                    break;
+                                default:
                                 throw new AssertionError();
+                            }
                         }
-                        break;
+                    break;
+                    case 3:
+                        boolean navegadorAtivo = true;
+                        while(navegadorAtivo){
+                            
+                        }
+                    break;
+                    case 0:
+                        System.out.println("Encerrando...");
+                    return;
                     default:
                         throw new AssertionError();
                 }
--- DesafioIphone/Iphone_POO/src/telefone/Telefone.java
+++ DesafioIphone/Iphone_POO/src/telefone/Telefone.java
@@ -2,10 +2,12 @@ package telefone;
 
 public class Telefone {
     public void ligar(){
-
+        System.out.println("ligando!");
     }
     public void atender(){
-
+        System.out.println("atender!");
     }
-    
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando Correio de voz");
+    }
 }
